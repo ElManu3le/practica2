98dd6c3a2d466252b78556b70645cb5878e94f47
ElManu3le <msancho620@yahoo.es>
2021-01-06 14:07:09 +0100

    changes not commit

--- ejercicio1/Builder/src/vehiculo/autobus.java
+++ ejercicio1/Builder/src/vehiculo/autobus.java
@@ -3,43 +3,46 @@ package vehiculo;
 public class autobus {
 
     public int pasajeros;
-    public String compañia;
+    public String compayia;
     public String tipo_bus;
 
-    public autobus(int pasajeros, String compañia, String tipo_bus) {
+    public autobus(int pasajeros, String compayia, String tipo_bus) {
         this.pasajeros = pasajeros;
-        this.compañia = compañia;
+        this.compayia = compayia;
         this.tipo_bus = tipo_bus;
     }
 
-    public int getPasajeros() {
-        return pasajeros;
-    }
+	public int getPasajeros() {
+		return pasajeros;
+	}
 
-    public void setPasajeros(int pasajeros) {
-        this.pasajeros = pasajeros;
-    }
+	public void setPasajeros(int pasajeros) {
+		this.pasajeros = pasajeros;
+	}
 
-    public String getCompañia() {
-        return compañia;
-    }
+	public String getCompayia() {
+		return compayia;
+	}
 
-    public void setCompañia(String compañia) {
-        this.compañia = compañia;
-    }
+	public void setCompayia(String compayia) {
+		this.compayia = compayia;
+	}
 
-    public String getTipo_bus() {
-        return tipo_bus;
-    }
+	public String getTipo_bus() {
+		return tipo_bus;
+	}
 
-    public void setTipo_bus(String tipo_bus) {
-        this.tipo_bus = tipo_bus;
-    }
+	public void setTipo_bus(String tipo_bus) {
+		this.tipo_bus = tipo_bus;
+	}
 
-    @Override
-    public String toString() {
-        return "autobus [compañia=" + compañia + ", pasajeros=" + pasajeros + ", tipo_bus=" + tipo_bus + "]";
-    }
+	@Override
+	public String toString() {
+		return "autobus [pasajeros=" + pasajeros + ", compayia=" + compayia + ", tipo_bus=" + tipo_bus + "]";
+	}
+    
+
+   
 
     
 
